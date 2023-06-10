8d1f2361b286789c2db81389afbe1078c33f263d
Bharath <kumarbharath63@gmail.com>
2023-06-10 16:32:00 +0530

    First version

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Bharath Kumar
+
+Software Engineer - who just don't want to code but do the engineering.
+
+🐦 Twitter: https://twitter.com/iam_pbk
+📚 Github:  https://github.com/Bharath-code
+
+`);
