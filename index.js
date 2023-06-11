8de90d0e1a09479ff25844172bc86886d03997c7
Bharath <kumarbharath63@gmail.com>
2023-06-11 20:31:48 +0530

    colors

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 #!/usr/bin/env node
 
-const welcome = require("cli-welcome")
-const pkgJSON = require("./package.json")
+import chalk from 'chalk';
+import welcome from "cli-welcome";
+import pkgJSON from "./package.json" assert { type: "json" };
 
+// Add import assertion
+
+//import pkgJSON from "./package.json"
+//const welcome = require("cli-welcome")
+//const pkgJSON = require("./package.json")
+const log = console.log;
+const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
+const purple = chalk.hex(`#6cc644`).bold.inverse;
+const italic = chalk.italic;
+const dim = chalk.dim;
 welcome(
   {
-    title: pkgJSON.name,
+    title: `Bharathkumar`,
     tagLine: `Hi, nice to meet you all`,
     version: pkgJSON.version,
     description: pkgJSON.description,
-    bgColor: `#FADC00`,
+    bgColor: `#6937FF`,
     color: `#000000`,
     bold: true,
     clear: true,
   }
 );
-console.log(`
-Bharath Kumar
+log(`
+${italic(
+  `Software Engineer - who just don't want to code but do the engineering.`
+)}
 
-Software Engineer - who just don't want to code but do the engineering.
-
-🐦 Twitter: https://twitter.com/iam_pbk
-📚 Github:  https://github.com/Bharath-code
+🐦 ${twitterClr(` Twitter `)} ${dim(`https://twitter.com/iam_pbk`)}
+📚 ${purple(` Github `)}  ${dim(`https://github.com/Bharath-code`)}
 
 `);
